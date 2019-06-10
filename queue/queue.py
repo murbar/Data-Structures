f81df0edd5fa6054cad8e27924e983d85d50d614
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-06-10 17:35:04 -0400

    Implement with linked list

--- queue/queue.py
+++ queue/queue.py
@@ -1,18 +1,61 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, node):
+        self.next_node = node
+
+    def set_value(self, value):
+        self.value = value
+
+
 class Queue:
     def __init__(self):
-        self.size = 0
-        # what data structure should we
-        # use to store queue elements?
-        self.storage = []
+        self.head = None
+        self.tail = None
 
     def enqueue(self, item):
-        self.storage.insert(0, item)
+        node = Node(item)
+
+        if not self.head and not self.tail:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.set_next(node)
+            self.tail = node
 
     def dequeue(self):
-        if self.len() == 0:
+        if not self.head and not self.tail:
             return None
 
-        return self.storage.pop()
+        if self.head is self.tail:
+            item = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return item
+
+        new_head = self.head.get_next()
+        item = self.head.get_value()
+        self.head = new_head
+        return item
 
     def len(self):
-        return len(self.storage)
+        if not self.head and not self.tail:
+            return 0
+
+        if self.head is self.tail:
+            return 1
+
+        n = 1
+        element = self.head
+        while element.get_next():
+            n += 1
+            element = element.get_next()
+        return n
